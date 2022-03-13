d556c0436cdfb60a0a69439c5e699ac7af67d2aa
Melvinthg <mtmelvin63@gmail.com>
2022-03-13 22:19:55 +0800

    descriptions for landing page

--- src/pages/Landing.vue
+++ src/pages/Landing.vue
@@ -1,7 +1,26 @@
 <template>
-    <div>
-        <h1>Landing</h1>
+
+    <div class="landingpage">
+        <div class = "description">
+        <h1> WELCOME </h1>
+        <h2> to our child care center </h2>
+        <h4> providing personalised care and attention for your little ones </h4>
+        <br>
+        </div>
     </div>
+
+        <div class = "announcements">
+            <h2> Announcements </h2>
+            <h5> 31/01 Chinese new year's eve half day closure </h5>
+            <h5> 01/02 - 02/02 Chinese new year public holiday closure </h5>
+            <h5> 16/02 Excursion to Science Center Singapore </h5>
+            <h5> 23/02 Carnival Day 2022 </h5>
+        </div>
+
+        <div class = "gallery">
+            <h2> Our Gallery </h2>
+        </div>
+
 </template>
 
 <script>
@@ -11,5 +30,19 @@
 </script>
 
 <style scoped>
+
+
+.announcem ents {
+  position: left;
+  width:fit-content;
+  height: 100%;
+  background: #777;
+  text-align: centre;
+  color: rgb(0, 0, 0);
+  float: right;
+  line-height: 40px;
+  padding-left: 25px;
+  padding-right: 25px
+}
 
 </style>
