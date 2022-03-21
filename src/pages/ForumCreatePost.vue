78b2e7732058b46b0ab34b8c421258d942fd0587
joshualah <e0560196@u.nus.edu>
2022-03-21 18:06:07 +0800

    commit

--- src/pages/ForumCreatePost.vue
+++ src/pages/ForumCreatePost.vue
@@ -89,29 +89,29 @@ export default {
         location.href = "/forumdisplay";
     },
     click1() {
-    this.$refs.input1.click()   
+        this.$refs.input1.click()   
     },
     previewImage(event) {
-    this.uploadValue=0;
-    this.img1=null;
-    this.imageData = event.target.files[0];
-    // this.onUpload()
+        this.uploadValue=0;
+        this.img1=null;
+        this.imageData = event.target.files[0];
+        // this.onUpload()
     },
     onUpload(){
-    this.img1=null;
-    const storageRef = firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
-    storageRef.on(`state_changed`,snapshot=>{
-    this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
-        }, error=>{console.log(error.message)},
-    ()=>{this.uploadValue=100;
-        storageRef.snapshot.ref.getDownloadURL().then((url)=>{
-            this.img1 = url;
-            console.log(this.img1)
+        this.img1=null;
+        const storageRef = firebase.storage().ref(`${this.imageData.name}`).put(this.imageData);
+        storageRef.on(`state_changed`,snapshot=>{
+        this.uploadValue = (snapshot.bytesTransferred/snapshot.totalBytes)*100;
+            }, error=>{console.log(error.message)},
+            ()=>{this.uploadValue=100;
+            storageRef.snapshot.ref.getDownloadURL().then((url)=>{
+                this.img1 = url;
+                console.log(this.img1)
             });
         }      
         );
     },
-    }
+  }
 }
 </script>
 <style>
