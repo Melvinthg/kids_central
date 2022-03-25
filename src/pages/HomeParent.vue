4dd39d6b524c9779a34c157f0d5b4f6ec22f911f
tammehh <tammyypx@gmail.com>
2022-03-25 17:32:40 +0800

    commiting to pull

--- src/pages/HomeParent.vue
+++ src/pages/HomeParent.vue
@@ -4,44 +4,26 @@
       <el-container>
         <el-aside width="200px" style="background-color: cornflowerblue">
           <br />
-          <header style="text-align:center; size=18px">{{ user.email }}</header>
+          <header style="text-align:center; size=18px">{{user.email}}</header>
           <br />
           <el-menu
             active-text-color="steelblue"
             background-color="dodgerblue"
             textcolor="white"
           >
-            <el-menu-item v-if="this.homeType == 'teacher'" index="1">
+            <el-menu-item index="1">
               <el-icon><Edit /></el-icon>
               <span
-
                 ><router-link to="/editclassdashboard" className="sidebarLinks"
-
                   >Edit Class Dashboard</router-link
                 ></span
               >
             </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'parent'" index="1a">
-              <el-icon><Edit /></el-icon>
-              <span
-                ><router-link to="/editclassdashboard" className="sidebarLinks"
-                  >View Child Dashboard</router-link
-                ></span
-              >
-            </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'teacher'" index="2">
-              <el-icon><Notebook /></el-icon>
-              <span
-                ><router-link to="/ChildrenInfo" className="sidebarLinks"
-                  >Manage Class Info</router-link
-                ></span
-              >
-            </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'parent'" index="2a">
+            <el-menu-item index="2">
               <el-icon><Notebook /></el-icon>
               <span
                 ><router-link to="/home" className="sidebarLinks"
-                  >View Child Info</router-link
+                  >Manage Class Info</router-link
                 ></span
               >
             </el-menu-item>
@@ -53,36 +35,23 @@
                 ></span
               >
             </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'teacher'" index="4">
+            <el-menu-item index="4">
               <el-icon><Cellphone /></el-icon>
               <span
-                ><router-link to="/contactparent" className="sidebarLinks"
-                  >Contact Parents</router-link
+                ><router-link to="/contactteacher" className="sidebarLinks"
+                  >Contact Teachers</router-link
                 ></span
               >
             </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'parent'" index="4a">
-              <el-icon><Cellphone /></el-icon>
-              <span
-                ><router-link to="/contactparent" className="sidebarLinks"
-                  >Contact Teacher</router-link
-                ></span
-              >
-            </el-menu-item>
-             <!-- using this line to test -->
-              <span><router-link to="/HealthAndInjuries" className="sidebarLinks">testingforHealthinjuries</router-link></span><br>
-              <span><router-link to="/CognitiveAbilities" className="sidebarLinks">CognitiveAbilities</router-link></span>
-
           </el-menu>
         </el-aside>
         <el-main>
-          <div v-if="this.homeType == 'teacher'" class="writepost">
+          <div class="writepost">
             <WritePost></WritePost>
           </div>
           <br /><br />
           <div class="feed">
             <h1>feed</h1>
-            <GetPost></GetPost>
           </div>
         </el-main>
       </el-container>
@@ -93,7 +62,6 @@
 
 <script>
 import WritePost from "@/components/WritePost.vue";
-import GetPost from "@/components/GetPost.vue";
 import {
   Edit,
   Notebook,
@@ -102,13 +70,19 @@ import {
 } from "@element-plus/icons-vue";
 import { getAuth } from "firebase/auth";
 const auth = getAuth();
+//import {Location,Document,Menu as IconMenu,Setting} from '@element-plus/icons-vue'
+
+// import firebaseApp from "../firebase.js";
+// import { getFirestore } from "firebase/firestore";
+// import { doc } from "firebase/firestore";
+// const db = getFirestore(firebaseApp);
 
 export default {
-  name: "Home",
+  name: "HomeParent",
   data() {
     return {
       user: auth.currentUser,
-      homeType: ""
+      
     };
   },
   components: {
@@ -117,22 +91,27 @@ export default {
     Notebook,
     ChatLineSquare,
     Cellphone,
-    GetPost,
-  },
-  created: function () {
-    this.homeType = this.$store.state.userModel.type,
-    console.log(this.homeType);
   },
 };
 </script>
 
 <style>
 @import url("https://fonts.googleapis.com/css2?family=Inter&display=swap");
+
 .sidebarLinks {
   color: white;
   font-family: Arial, Helvetica, sans-serif;
   font-size: 14px;
   text-decoration: none;
+}
+
+.text-link:hover {
+  padding-left: 5px;
+}
+
+.content {
+  position: fixed;
+  margin-left: 30%;
 }
 .writepost {
   padding: 10px;
@@ -140,6 +119,7 @@ export default {
   height: auto;
   background: #eeeeee;
 }
+
 .feed {
   margin-left: 25%;
   width: fit-content;
--- src/pages/HomeTeacher.vue
+++ src/pages/HomeTeacher.vue
@@ -11,37 +11,19 @@
             background-color="dodgerblue"
             textcolor="white"
           >
-            <el-menu-item v-if="this.homeType == 'teacher'" index="1">
+            <el-menu-item index="1">
               <el-icon><Edit /></el-icon>
               <span
-
                 ><router-link to="/editclassdashboard" className="sidebarLinks"
-
                   >Edit Class Dashboard</router-link
                 ></span
               >
             </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'parent'" index="1a">
-              <el-icon><Edit /></el-icon>
-              <span
-                ><router-link to="/editclassdashboard" className="sidebarLinks"
-                  >View Child Dashboard</router-link
-                ></span
-              >
-            </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'teacher'" index="2">
+            <el-menu-item index="2">
               <el-icon><Notebook /></el-icon>
               <span
                 ><router-link to="/ChildrenInfo" className="sidebarLinks"
                   >Manage Class Info</router-link
-                ></span
-              >
-            </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'parent'" index="2a">
-              <el-icon><Notebook /></el-icon>
-              <span
-                ><router-link to="/home" className="sidebarLinks"
-                  >View Child Info</router-link
                 ></span
               >
             </el-menu-item>
@@ -53,7 +35,7 @@
                 ></span
               >
             </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'teacher'" index="4">
+            <el-menu-item index="4">
               <el-icon><Cellphone /></el-icon>
               <span
                 ><router-link to="/contactparent" className="sidebarLinks"
@@ -61,28 +43,26 @@
                 ></span
               >
             </el-menu-item>
-            <el-menu-item v-if="this.homeType == 'parent'" index="4a">
-              <el-icon><Cellphone /></el-icon>
-              <span
-                ><router-link to="/contactparent" className="sidebarLinks"
-                  >Contact Teacher</router-link
-                ></span
-              >
-            </el-menu-item>
-             <!-- using this line to test -->
-              <span><router-link to="/HealthAndInjuries" className="sidebarLinks">testingforHealthinjuries</router-link></span><br>
-              <span><router-link to="/CognitiveAbilities" className="sidebarLinks">CognitiveAbilities</router-link></span>
-
+            <!-- using this line to test -->
+            <span
+              ><router-link to="/HealthAndInjuries" className="sidebarLinks"
+                >testingforHealthinjuries</router-link
+              ></span
+            ><br />
+            <span
+              ><router-link to="/CognitiveAbilities" className="sidebarLinks"
+                >CognitiveAbilities</router-link
+              ></span
+            >
           </el-menu>
         </el-aside>
         <el-main>
-          <div v-if="this.homeType == 'teacher'" class="writepost">
+          <div class="writepost">
             <WritePost></WritePost>
           </div>
           <br /><br />
           <div class="feed">
             <h1>feed</h1>
-            <GetPost></GetPost>
           </div>
         </el-main>
       </el-container>
@@ -93,7 +73,6 @@
 
 <script>
 import WritePost from "@/components/WritePost.vue";
-import GetPost from "@/components/GetPost.vue";
 import {
   Edit,
   Notebook,
@@ -102,13 +81,16 @@ import {
 } from "@element-plus/icons-vue";
 import { getAuth } from "firebase/auth";
 const auth = getAuth();
-
+//import {Location,Document,Menu as IconMenu,Setting} from '@element-plus/icons-vue'
+// import firebaseApp from "../firebase.js";
+// import { getFirestore } from "firebase/firestore";
+// import { doc } from "firebase/firestore";
+// const db = getFirestore(firebaseApp);
 export default {
-  name: "Home",
+  name: "HomeTeacher",
   data() {
     return {
       user: auth.currentUser,
-      homeType: ""
     };
   },
   components: {
@@ -117,11 +99,6 @@ export default {
     Notebook,
     ChatLineSquare,
     Cellphone,
-    GetPost,
-  },
-  created: function () {
-    this.homeType = this.$store.state.userModel.type,
-    console.log(this.homeType);
   },
 };
 </script>
@@ -133,6 +110,13 @@ export default {
   font-family: Arial, Helvetica, sans-serif;
   font-size: 14px;
   text-decoration: none;
+}
+.text-link:hover {
+  padding-left: 5px;
+}
+.content {
+  position: fixed;
+  margin-left: 30%;
 }
 .writepost {
   padding: 10px;
