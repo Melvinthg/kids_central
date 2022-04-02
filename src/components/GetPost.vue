8f54ae3a9bcc4b6965a0f4271be15e2dcbdf50fb
tammehh <tammyypx@gmail.com>
2022-04-02 16:43:03 +0800

    stuff

--- src/components/GetPost.vue
+++ src/components/GetPost.vue
@@ -4,15 +4,14 @@
       <el-card
         v-for="post in posts"
         :key="post.id"
-        :body-style="{ padding: '0px', width: auto}"
-
+        :body-style="{ padding: '0px', width: auto }"
       >
         <img v-bind:src="'{{post.imageURL}}'" class="image" />
         <div style="padding: 14px">
-          <span>{{post.poster}} shared with you  </span>
-        <time class="time">{{ post.date }}</time>       
+          <span>{{ post.poster }} shared with you </span>
+          <time class="time">{{ post.date }}</time>
           <div class="bottom">
-          <span>{{ post.caption }}</span>  
+            <span>{{ post.caption }}</span>
           </div>
         </div>
       </el-card>
@@ -22,7 +21,7 @@
 
 <script>
 import { db } from "../firebase.js";
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs, where, query, orderBy } from "firebase/firestore";
 //import { getStorage, } from "firebase/storage";
 export default {
   name: "GetPost",
@@ -33,19 +32,30 @@ export default {
   },
   methods: {
     async getPosts() {
-      console.log("got the post");
-      let value = await getDocs(collection(db, "posts"));
-      console.log("got the post");
-      value.forEach((d) => {
-        this.posts.push({
-          id: d.id,
-          caption: d.data().caption,
-          //date: new Date(d.data().date.seconds*1000),
-          date: d.data().date.toDate().toString().slice(4,21),
-          imageUrl: d.data().imageUrl,
-          location: d.data().location,
-          poster: d.data().poster,
-          recipient: d.data().recipient,
+      console.log(this.$store.state.userModel);
+      let q;
+      if (this.$store.state.userModel.type == "parent") {
+        q = query(
+          collection(db, "posts"),
+          where("recipient", "==", this.$store.state.userModel.childID),
+          orderBy("date", "desc")
+        );
+      } else {
+        q = query(collection(db, "posts"), orderBy("date", "desc"));
+      }
+
+      getDocs(q).then((res) => {
+        res.forEach((d) => {
+          this.posts.push({
+            id: d.id,
+            caption: d.data().caption,
+            //date: new Date(d.data().date.seconds*1000),
+            date: d.data().date.toDate().toString().slice(4, 21),
+            imageUrl: d.data().imageUrl,
+            location: d.data().location,
+            poster: d.data().poster,
+            recipient: d.data().recipient,
+          });
         });
       });
     },
